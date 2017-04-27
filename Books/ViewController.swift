aa4efbe2e54a846b3c2752369fd137ae3969ed5f
SDS-019 <sds-019@70-12-244-200.pools.spcsdns.net>
2017-04-27 13:15:52 +0900

    add

--- Books/ViewController.swift
+++ Books/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        openAlert()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func openAlert(){
+        let alert = UIAlertController(title: "Welcom", message: "Start Book App", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default, handler: nil)
+        
+        /*
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: {
+            (UIAlertAction)->() in
+            self.view.backgroundColor = UIColor.red
+        })
+ */
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel){
+            (UIAlertAction)->() in
+            self.view.backgroundColor = UIColor.red
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true){
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: false, block: {
+                (Timer)->Void in
+                alert.dismiss(animated: true, completion: nil)
+            })
+
+        }
+        
+    }
 
 }
 
